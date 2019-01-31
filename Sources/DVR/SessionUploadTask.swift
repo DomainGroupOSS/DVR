30f067d10a48d2d65c3096f8e8402ea14dbf66ee
Dan Rodrigues <dan.rodrigues@domain.com.au>
2019-01-31 17:25:09 +1100

    Quick and dirty taskIdentifier overrides

--- Sources/DVR/SessionUploadTask.swift
+++ Sources/DVR/SessionUploadTask.swift
@@ -10,16 +10,22 @@ final class SessionUploadTask: URLSessionUploadTask {
 
     weak var session: Session!
     let request: URLRequest
+    private let injectedIdentifier: Int
     let completion: Completion?
     let dataTask: SessionDataTask
 
+    override var taskIdentifier: Int {
+        return injectedIdentifier
+    }
+
     // MARK: - Initializers
 
-    init(session: Session, request: URLRequest, completion: Completion? = nil) {
+    init(session: Session, request: URLRequest, taskIdentifier: Int, completion: Completion? = nil) {
         self.session = session
         self.request = request
+        self.injectedIdentifier = taskIdentifier
         self.completion = completion
-        dataTask = SessionDataTask(session: session, request: request, completion: completion)
+        dataTask = SessionDataTask(session: session, request: request, taskIdentifier: session.nextTaskIdentifier(), completion: completion)
     }
 
     // MARK: - URLSessionTask
